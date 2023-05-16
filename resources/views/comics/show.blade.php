931e1eaabb8b9c32b58492bb6da616d9b37e5d8c
danilopiccinni <danilopiccinni@gmail.com>
2023-05-16 15:22:58 +0200

    feat: show details single comic (partial style)

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -10,15 +10,57 @@
 
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+              <h5 class="card-title">{{ $comic->title }}</h5>
+              <p class="card-text">{{ $comic->description }}</p>
             </div>
           </div>
           <div class="col-md-4">
-            <img src="..." class="img-fluid rounded-start" alt="...">
+            <img src="{{$comic->thumb}}" class="img-fluid rounded-start" alt="...">
           </div>
         </div>
       </div>
+</div>
+
+<div>
+    <div class="container">
+        <div>
+            <table>
+                <thead>
+                    <th>Talent</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Art by:</td>
+                        <td> {{ $comic->artists}} </td>
+                    </tr>
+                    <tr>
+                        <td>Written by:</td>
+                        <td>{{ $comic->writers }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div>
+            <table>
+                <thead>
+                    <th>Specs</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Series:</td>
+                        <td> {{ $comic->series}} </td>
+                    </tr>
+                    <tr>
+                        <td>U.S. Price:</td>
+                        <td>{{ $comic->price }}</td>
+                    </tr>
+                    <tr>
+                        <td>On Sale Date:</td>
+                        <td>{{ $comic->sale_date }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 @endsection
