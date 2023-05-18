a3c27d7c76a70e2c0257e29006a438563be606d5
danilopiccinni <danilopiccinni@gmail.com>
2023-05-18 16:48:01 +0200

    feat: active_url validate

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -66,6 +66,18 @@ class ComicController extends Controller
 
         $this->validatorDataForm($request);
     
+        // $request->validate([
+        //     'title' => 'required|max:50|min:2',
+        //     'description' => 'required|max:50|min:2',
+        //     'thumb' => 'required|max:50|min:2|active_url',
+        //     'price' => 'required|max:50|min:2',
+        //     'series' => 'required|max:50|min:2',
+        //     'sale_date' => 'required|max:50|min:2',
+        //     'type' => 'required|max:50|min:2',
+        //     'artists' => 'required|max:300|min:1',
+        //     'writers' => 'required|max:300|min:1',
+        // ]);
+
         $formData = $request->all();
 
         $newComic = new Comic();
@@ -178,7 +190,7 @@ class ComicController extends Controller
         $validator = Validator::make($formData, [
             'title' => 'required|max:50|min:2',
             'description' => 'required|max:300|min:2',
-            'thumb' => 'required|max:50|min:2',
+            'thumb' => 'required|max:50|min:2|active_url',
             'price' => 'required|max:50|min:2',
             'series' => 'required|max:50|min:2',
             'sale_date' => 'required|max:50|min:2',
@@ -187,32 +199,33 @@ class ComicController extends Controller
             'writers' => 'required|max:300|min:2',
         ], [
             'title.required' => 'inserisci il titolo, non puoi lasciare questo campo vuoto',
-            'title' => 'lunghezza caratteri massima 50',
-            'title' => 'lunghezza caratteri minima 2',
+            'title.max' => 'lunghezza caratteri massima 50',
+            'title.min' => 'lunghezza caratteri minima 2',
             'description.required' => 'inserisci la descrizione, non puoi lasciare questo campo vuoto',
             'description' =>  'lunghezza caratteri massima 300',
             'description' => 'lunghezza caratteri minima 2',
             'thumb.required' => 'inserisci il link, non puoi lasciare questo campo vuoto',
-            'thumb' => 'lunghezza caratteri massima 50',
-            'thumb' => 'lunghezza caratteri minima 2',
+            'thumb.max' => 'lunghezza caratteri massima 50',
+            'thumb.min' => 'lunghezza caratteri minima 2',
+            'thumb.active_url' => 'inserisci un link valido',
             'price.required' => 'inserisci il prezzo, non puoi lasciare questo campo vuoto',
-            'price' => 'lunghezza caratteri massima 50',
-            'price' => 'lunghezza caratteri minima 2',
+            'price.max' => 'lunghezza caratteri massima 50',
+            'price.min' => 'lunghezza caratteri minima 2',
             'series.required' => 'inserisci la serie, non puoi lasciare questo campo vuoto',
-            'series' => 'lunghezza caratteri massima 50',
-            'series' => 'lunghezza caratteri minima 2',
+            'series.max' => 'lunghezza caratteri massima 50',
+            'series.min' => 'lunghezza caratteri minima 2',
             'sale_date.required' => 'inserisci la data, non puoi lasciare questo campo vuoto',
-            'sale_date' => 'lunghezza caratteri massima 50',
+            'sale_date.max' => 'lunghezza caratteri massima 50',
             'sale_date' => 'lunghezza caratteri minima 2',
             'type.required' => 'inserisci la tipologia, non puoi lasciare questo campo vuoto',
-            'type' => 'lunghezza caratteri massima 50',
-            'type' => 'lunghezza caratteri minima 2',
+            'type.max' => 'lunghezza caratteri massima 50',
+            'type.min' => 'lunghezza caratteri minima 2',
             'artists.required' => 'inserisci almeno un "artist", non puoi lasciare questo campo vuoto',
-            'artists' =>  'lunghezza caratteri massima 300',
-            'artists' => 'lunghezza caratteri minima 2',
+            'artists.max' =>  'lunghezza caratteri massima 300',
+            'artists.min' => 'lunghezza caratteri minima 2',
             'writers.required' => 'inserisci almeno un "writer" non puoi lasciare questo campo vuoto',
-            'writers' =>  'lunghezza caratteri massima 300',
-            'writers' => 'lunghezza caratteri minima 2',
+            'writers.max' =>  'lunghezza caratteri massima 300',
+            'writers.min' => 'lunghezza caratteri minima 2',
 
 
         ])->validate();
